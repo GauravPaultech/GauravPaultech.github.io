9a3065406bfe396d31076a2191c0f8eca6c74353
GauravPaultech <gauravpaul801@gmail.com>
2026-07-14 14:08:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ sections.forEach((section)=>{
     section.classList.add("hidden");
     observer.observe(section);
 });
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    document.querySelectorAll("section").forEach((section) => {
+
+        const sectionTop = section.offsetTop - 120;
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach((link) => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
